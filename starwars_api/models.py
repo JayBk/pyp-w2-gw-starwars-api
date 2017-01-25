f482ac38df570f05b86b6aa3b566fb9a90c596c1
Josh Kopp <jkopp5549@gmail.com>
2017-01-25 18:54:04 +0000

    solution W/O comments

--- starwars_api/models.py
+++ starwars_api/models.py
@@ -31,8 +31,8 @@ class BaseModel(object):
         later in charge of performing requests to SWAPI for each of the
         pages while looping.
         """
-        query = '{}QuerySet'.format(cls.__name__)  #cls.__name__ == either "People" or "Films", so instead of doing cls.RESOURCE_NAME.title() we can use this... 
-        return eval(query)()                       # RESOURCE_NAME == "people" or "films" to you'd have to do .tile() and make the first letter capital to get the actual class name.
+        query = '{}QuerySet'.format(cls.__name__)
+        return eval(query)()                       
                                       
 
 class People(BaseModel):
@@ -60,7 +60,7 @@ class BaseQuerySet(object):
 
     def __init__(self):
         self.objects = []        
-        self.page, self.counter = 0, 0  # We need to +1 to self.counter when checking it with len(self.objects). Otherwise it will be > because we are always adding 1 it at the end of the while loop, and then checking it before we add another 
+        self.page, self.counter = 0, 0 
         self.for_count = None
 
     def __iter__(self):
@@ -72,8 +72,8 @@ class BaseQuerySet(object):
         page were all consumed.
         """
         while self:  # (while True)
-            if self.counter+1 > len(self.objects):  # We need to check if we +1 to self.element that if it is > the length of self.object, we try to get the next page, but except the apierror, and
-                try: # if we do end up getting an error, stop the iteration. I don't think we were handline the errors right before :/
+            if self.counter+1 > len(self.objects): 
+                try: 
                     self.get_next_page()
                 except SWAPIClientError:
                     raise StopIteration()
@@ -86,11 +86,11 @@ class BaseQuerySet(object):
         next_page = 'get_{}'.format(self.RESOURCE_NAME)
         json_data = getattr(api_client, next_page)(page=self.page)
         self.no_inst_results = json_data['results']
-        self.for_count = json_data['count']  # We can use this For the count() function or use return len([item for item in self])
-        make_obj  = eval(self.RESOURCE_NAME.title())  # Easiest way is to use the RESOURCE_NAME and make the first letter capital which will make it People() or Films(), 
-        for data in json_data['results']:           # Here we iterate through json_data['results'] taking each entry and appending it to a list, and while we do that, we instantiate it so that
-            inst_data = make_obj(data)          # This is what I meant when we first started to save 
-            self.objects.append(inst_data)  # this way we will not get the AttributeError like we were before, because now were going through everything in results and calling People(data) or Films(data) on it so that it ends up having the attributes...
+        self.for_count = json_data['count']  
+        make_obj  = eval(self.RESOURCE_NAME.title())  
+        for data in json_data['results']:           
+            inst_data = make_obj(data)         
+            self.objects.append(inst_data)  
 
     next = __next__
 
@@ -100,11 +100,9 @@ class BaseQuerySet(object):
         If the counter is not persisted as a QuerySet instance attr,
         a new request is performed to the API in order to get it.
         """
-        # literally just have to do EXACTLY what it says above^^^...
-        if self.for_count is None:  #  If the counter is not persisted as a QuerySet instance attr,
-            self.get_next_page()  # a new request is performed to the API in order to get it.
-        return self.for_count  #  Returns the total count of objects of current model.
-        # or we can do: return len([item for item in self])
+        if self.for_count is None:  
+            self.get_next_page() 
+        return self.for_count  
 
 
 class PeopleQuerySet(BaseQuerySet):
